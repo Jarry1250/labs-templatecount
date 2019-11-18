3cf35a69706d897d1b923594f07f4b650b9989a1
Keith Brian Tomo <56818052+keithbrianptomo@users.noreply.github.com>
2019-11-18 14:06:14 +0800

    Revert "Provide basic error messages where input validation fails"

--- index.php
+++ index.php
@@ -28,17 +28,9 @@
 	$namespace = ( isset( $_GET['namespace'] ) && $_GET["namespace"] != "" ) ? htmlspecialchars( $_GET['namespace'] ) : 10; //10 is template namespace
 	$templateName = ( isset( $_GET['name'] ) && $_GET["name"] != "" ) ? str_replace( "_", " ", htmlspecialchars( $_GET['name'], ENT_QUOTES ) ) : '';
 
-
+	if( !preg_match( "/^[a-z-]{2,8}$/", $language ) ) die(); // Safety precaution
+	if( !is_numeric( $namespace ) ) die(); // Safety precaution
 	echo get_html( 'header', 'Template transclusion count' );
-
-	if( !preg_match( "/^[a-z-]{2,8}$/", $language ) ) { // Safety precaution
-		echo '<b>Error:</b> Language parameter with invaid format.<br>';
-		die( get_html( 'footer' ) );
-	}
-	if( !is_numeric( $namespace ) ) { // Safety precaution
-		echo '<b>Error:</b> Namespace parameter must be numerical.<br>';
-		die( get_html( 'footer' ) );
-	}
 ?>
 		<h3><?php echo _html( 'enter-details' ); ?></h3>
 		<p><?php echo _html( 'introduction' ); ?></p>
